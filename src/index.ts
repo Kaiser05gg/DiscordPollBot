cf244e8f1f128ffd80b3101147534c482082d012
Kaiser05gg <shu.iss1121@icloud.com>
2025-10-28 13:43:09 +0900

    index.tsの不要インポートを整理し、クリーンアーキテクチャ構成を最終確定

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
-/// <reference path="./types/discord.d.ts" />
-
-import {
-  Client,
-  GatewayIntentBits,
-  REST,
-  Routes,
-  Interaction,
-  VoiceState,
-} from "discord.js";
-import { createPoll } from "./application/usecases/createPoll.js";
-import { client } from "./infrastructure/discord/discordClient.js";
-import { schedulePoll } from "./application/services/schedulePoll.js";
-import { registerCommands } from "./application/usecases/registerCommands.js";
+import "dotenv/config";
 import { startExpressServer } from "./interfaces/http/server.js";
+import { client } from "./infrastructure/discord/discordClient.js";
 import { ensureTables } from "./infrastructure/mysql/schema.js";
+import { registerCommands } from "./application/usecases/registerCommands.js";
+import { schedulePoll } from "./application/services/schedulePoll.js";
 import { setupPollListeners } from "./application/usecases/pollVoteHandlers.js";
 import { setupInteractionHandlers } from "./application/usecases/interactionHandlers.js";
-import { pollRepository } from "./infrastructure/mysql/pollRepository.js";
-import { Events, MessagePollVoteAdd, MessagePollVoteRemove } from "discord.js"; //client.on(Events.MessagePollVoteAdd, async (vote: any)の関数。現在後回しにしている。
-import { config } from "dotenv";
-import cron from "node-cron";
-config();
 
 startExpressServer();
 
