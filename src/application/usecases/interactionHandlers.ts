011f8483281337eaf8f254cfbfe1b3e88e1d4a8c
Kaiser05gg <shu.iss1121@icloud.com>
2025-11-04 11:53:10 +0900

    fix(import): correct pythonExecutor path from analytics to infrastructure

--- src/application/usecases/interactionHandlers.ts
+++ src/application/usecases/interactionHandlers.ts
@@ -1,6 +1,6 @@
 import { Client, Interaction, AttachmentBuilder } from "discord.js";
 import { createPoll } from "./createPoll.js";
-import { generateGraph } from "../../analytics/pythonExecutor.js";
+import { runPythonScript as generateGraph } from "../../infrastructure/python/pythonExecutor.js";
 
 export const setupInteractionHandlers = (client: Client) => {
   client.on("interactionCreate", async (interaction: Interaction) => {
