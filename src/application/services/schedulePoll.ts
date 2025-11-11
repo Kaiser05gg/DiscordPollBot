9743ab169a18f1cb07cb8996d315e07816e28050
Kaiser05gg <shu.iss1121@icloud.com>
2025-11-11 12:42:22 +0900

    add 1分ごとのPoll結果反映

--- src/application/services/schedulePoll.ts
+++ src/application/services/schedulePoll.ts
@@ -1,6 +1,7 @@
 import cron from "node-cron";
 import { Client } from "discord.js";
-import { createPoll } from "../usecases/createPoll.js";
+import { createPoll } from "../usecases/createPoll";
+import { updatePollResultUseCase } from "../usecases/updatePollResultUseCase";
 
 export const schedulePoll = (client: Client) => {
   const channelId = process.env.CHANNEL_ID;
@@ -12,6 +13,22 @@ export const schedulePoll = (client: Client) => {
       console.log("✅ JST12:00 定時投票を送信しました");
     } catch (err) {
       console.error("❌ 自動投票エラー:", err);
+    }
+  });
+  cron.schedule("* * * * *", async () => {
+    try {
+      const channel = await client.channels.fetch(channelId);
+      if (!channel?.isTextBased()) return;
+
+      const messages = await channel.messages.fetch({ limit: 10 });
+      for (const msg of messages.values()) {
+        if (!msg.poll) continue;
+        await updatePollResultUseCase(msg.poll);
+      }
+
+      console.log("✅ 1分ごとのPoll結果反映完了");
+    } catch (err) {
+      console.error("❌ Poll結果更新ジョブエラー:", err);
     }
   });
 };
