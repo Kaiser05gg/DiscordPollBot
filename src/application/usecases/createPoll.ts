29ebb2d5ed1acad2976bfe51d817faa7f7ff5d40
Kaiser05gg <shu.iss1121@icloud.com>
2025-11-15 22:45:39 +0900

    createpoll.tsのコメントアウトを削除

--- src/application/usecases/createPoll.ts
+++ src/application/usecases/createPoll.ts
@@ -6,7 +6,6 @@ export const createPoll = async (client: Client, channelId: string) => {
   if (!channel?.isTextBased())
     throw new Error("❌ 指定チャンネルがテキストチャンネルではありません");
 
-  // DiscordにPollを送信
   const message = await channel.send({
     poll: {
       question: { text: "本日のVALORANT" },
