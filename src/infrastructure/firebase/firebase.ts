b70e13a25d5c91c4104c54de5a19a2afa3d79c4e
Kaiser05gg <shu.iss1121@icloud.com>
2025-11-16 00:45:13 +0900

    fix: Firebase private key Base64 decode support

--- src/infrastructure/firebase/firebase.ts
+++ src/infrastructure/firebase/firebase.ts
@@ -8,11 +8,18 @@ dotenv.config({
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const privateKey = Buffer.from(
+    process.env.FIREBASE_PRIVATE_KEY_BASE64!,
+    "base64"
+  )
+    .toString("utf-8")
+    .replace(/\\n/g, "\n");
+
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey,
     }),
   });
 }
